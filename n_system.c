2cc522bf0a09d74ad02c00f5ef1bf7690b6d7a7e
SE-MohamedEissa <eissa.mohamed852@gmail.com>
2023-09-19 19:32:38 +0300

    kman mo7awla l task 1

--- n_system.c
+++ n_system.c
@@ -8,16 +8,16 @@
 int p_uns(va_list list)
 {
 	unsigned int number = va_arg(list, unsigned int);
-        char num[1024];
-        int i = 0, c = 0;
+	char num[1024];
+	int i = 0, c = 0;
 
-        if (number == 0)
-                num[i] = '0', i++;
-        if (number < 0)
-                _putchar('-'), number *= -1, c++;
-        while (number > 0)
-                num[i] = '0' + (number % 10), number /= 10, i++;
-        while (i > 0)
-                _putchar(num[--i]), c++;
-        return (c);
+	if (number == 0)
+		num[i] = '0', i++;
+	if (number < 0)
+		_putchar('-'), number *= -1, c++;
+	while (number > 0)
+		num[i] = '0' + (number % 10), number /= 10, i++;
+	while (i > 0)
+		_putchar(num[--i]), c++;
+	return (c);
 }
